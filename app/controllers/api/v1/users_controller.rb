34d683b8097e5a7975e11c959381687f2abae697
Nikkie Grom <nickonizator@gmail.com>
2020-01-12 15:29:48 +0300

    add api/v1 namespace

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Api::V1
+  class UsersController < ApplicationController
+    def index; end
+
+    def create; end
+
+    def update; end
+
+    def destroy; end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/create'
-  get 'users/update'
-  get 'users/destroy'
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: %i[index create update destroy]
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
